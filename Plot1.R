0a4ade91167320921d065b84153b646f5793f74a
Tiago Vaz <tiagosoaresmkt@gmail.com>
2017-07-03 13:23:44 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -2,17 +2,16 @@
 ## In my case, it will be determined by the path below
 
 setwd("C:/Users/tiago/Desktop/Coursera - Data Science/ProgrammingAssignment5")
-powerdata<-read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = F)
+powerdata<-read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = F, dec =".")
 
 ## We will only be using data from the dates 2007-02-01 and 2007-02-02. So the idea here is to subset 
 ## our dataset passing the dates as a vector.
 
 powerdata1 <- subset(powerdata, Date %in% c("1/2/2007","2/2/2007"))
-powerdata1$Global_active_power<-as.numeric(powerdata1$Global_active_power)
-powerdata1$Date <- as.Date(powerdata1$Date, format="%d/%m/%Y")
-hist(powerdata1$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-dev.copy(png, file="plot1.png", width=480, height=480)
-dev.off()    
+global_active_power<-as.numeric(powerdata1$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()  
 
 ## Final print out
 png 
